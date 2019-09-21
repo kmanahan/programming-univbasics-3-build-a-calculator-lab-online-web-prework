11fad0402f0484d7b4dcd7253bc1959ca5203702
congenial-language-8324 <kelly.a.manahan@gmail.com>
2019-09-21 16:36:28 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,42 @@
 def addition(num1, num2)
-
+puts num1 + num2
+return num1 + num2
 end
+
+addition(1,2)
+
 
 def subtraction(num1, num2)
-
+puts num1 - num2
+return num1 - num2
 end
+
+subtraction(2,1)
 
 def division(num1, num2)
-
+puts num1/num2
+return num1/num2
 end
+
+division(2,1)
 
 def multiplication(num1, num2)
-
+puts num1 * num2
+return num1 * num2
 end
+
+multiplication(2,1)
 
 def modulo(num1, num2)
-
+puts num1 % num2
+return num1 % num2
 end
+
+modulo(2,1)
 
 def square_root(num)
-
+puts Math.sqrt(num)
+return Math.sqrt(num)
 end
+
+square_root(4)
